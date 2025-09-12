99f719425480a74c454586186b75928f27c0c498
kalen17724-creator <kalen17724@gmail.com>
2025-09-12 03:16:40 -1100

    Week 2 programs
    Program 4 needs some help

--- program_1.py
+++ program_1.py
@@ -1,6 +1,10 @@
-def personal_information():
-    print('...')
+name = "Kalen Collins"
+address = "Saint Paul Minnesota 55653"
+phone_number = "(605) 272-2224"
+college_major = "Physcal Therapist" 
+print ("Hello my name is" , name) 
+print ("I live in", address)
+print ("my phone number is" , phone_number) 
+print ("and I will probably go to school to be a", college_major)
 
-
-# Line which calls the above function.
-personal_information()
+ 
